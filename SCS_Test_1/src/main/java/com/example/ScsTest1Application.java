11b83886788cbd4407447b5483d6e247712f46a9
Administrator <Administrator@SKCC16N01437.SKCC.NET>
2017-03-06 14:35:10 +0900

    g

---
+++ SCS_Test_1/src/main/java/com/example/ScsTest1Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScsTest1Application.class, args);
+	}
+}
