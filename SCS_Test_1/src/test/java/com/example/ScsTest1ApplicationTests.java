0547d4b8d8497d395b2a109524be2a78685f504e
Yeob <okayhan89@gmail.com>
2017-03-06 23:13:56 +0900

    GOGOGO

--- SCS_Test_1/src/test/java/com/example/ScsTest1ApplicationTests.java
+++ SCS_Test_1/src/test/java/com/example/ScsTest1ApplicationTests.java
@@ -1,11 +1,18 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import net.minidev.json.JSONObject;
 
 
 
@@ -14,7 +21,7 @@ import net.minidev.json.JSONObject;
 public class ScsTest1ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception, SQLException {
 		/*JSONObject obj = new JSONObject();
 
 		
@@ -31,9 +38,69 @@ public class ScsTest1ApplicationTests {
 		
 		System.out.println(obj.toString());
 		System.out.println(obj.toJSONString());*/
-		String jdbcUrl = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/ad_089f357c5f6b4e9?user=bbe6e5ccdab542&password=5025b3d1";
 		
+		String dbName = "ad_00b76e85c3373bf";
+		String userName = "bcdf2bac3b23de";
+		String password = "8bac561a";
+		String hostname = "us-cdbr-iron-east-04.cleardb.net";
+		String port = "3306";
+		String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?&user=" + userName + "&password="
+				+ password;
 		
+		//String jdbcUrl = "jdbc:mysql://us-cdbr-iron-east-04.cleardb.net/ad_089f357c5f6b4e9?user=bbe6e5ccdab542&password=5025b3d1";
+		
+		try {
+			System.out.println("Loading driver...");
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Cannot find the driver in the classpath!", e);
+		}
+		Connection con = null;
+		Statement setupStatement = null;
+		Statement readStatement = null;
+		ResultSet resultSet = null;
+		String results = "";
+		int numresults = 0;
+		String statement = null;
+
+		System.out.println("1");
+		con = DriverManager.getConnection(jdbcUrl);
+		String SQL = "select * from sample_table";
+		Statement stmt = (Statement) con.createStatement();
+		System.out.println("3");
+		ResultSet rs = stmt.executeQuery(SQL);
+
+		System.out.println("2");
+		ResultSetMetaData rsmd = null;
+		rsmd = rs.getMetaData();
+		int numberofColumn = rsmd.getColumnCount();
+
+		List<Float> row = null;
+		List<ArrayList<Float>> result = new ArrayList();
+
+		int count = 0;
+
+		while (rs.next()) {
+			row = new ArrayList<Float>();
+			for (int j = 1; j <= numberofColumn; j++) {
+
+				System.out.print(rs.getString(j) + "  ");
+				row.add(Float.parseFloat(rs.getString(j)));
+
+				System.out.println(row.toString());
+			}
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!");
+
+			result.add(count, (ArrayList<Float>) row);
+			count++;
+			System.out.println(count);
+			System.out.println(result.toString());
+
+		}
+
+		System.out.println(result.toString());
+
 	}
 
 }
